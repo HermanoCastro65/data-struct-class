353bbdda78d2beead09c69c0eca496128a0f0471
HermanoCastro65 <hermanocastro18@gmail.com>
2022-03-16 10:46:48 -0300

    class files from 2022 03 12

---
+++ classes/Aula20220312/Aula20220312/Aula20220312/Aula20220312.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+
+struct pessoa {
+    int idade;
+    char nome[40];
+};
+
+void bubble(int valores[], int tamanho) {
+    bool mudou = true;
+    while (mudou) {
+        mudou = false;
+        for (int i = 1; i < tamanho; i++) {
+            if (valores[i] < valores[i - 1]) {
+                mudou = true;
+                int aux = valores[i - 1];
+                valores[i - 1] = valores[i];
+                valores[i] = aux;
+            }
+        }
+    }
+}
+
+void bubble_pessoa(pessoa valores[], int tamanho) {
+    bool mudou = true;
+    while (mudou) {
+        mudou = false;
+        for (int i = 1; i < tamanho; i++) {
+            if (valores[i].idade < valores[i - 1].idade) {
+                mudou = true;
+                pessoa aux = valores[i - 1];
+                valores[i - 1] = valores[i];
+                valores[i] = aux;
+            }
+        }
+    }
+}
+
+int** muliplica(int** m1, int m, int k1, int** m2, int k2, int n) {
+    return NULL;
+}
+
+using namespace std;
+int main()
+{
+    pessoa vp1[3];
+    vp1[0].idade = 25;
+    strcpy_s(vp1[0].nome,"Ana");
+    vp1[1].idade = 64;
+    strcpy_s(vp1[1].nome, "Pedro");
+    vp1[2].idade = 12;
+    strcpy_s(vp1[2].nome, "Carlos");
+    bubble_pessoa(vp1, 3);
+    for (int i = 0; i < 3; i++) {
+        cout << vp1[i].nome << " : " << vp1[i].idade << "\n";
+    }
+
+    int m1[2][3] = { {2,5,7},
+                     {2,1,4} };
+
+    int m2[3][4] = { {1,2,3,4},
+                     {4,3,2,1},
+                     {1,2,3,4} };
+    
+    int resultado[2][4];
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 4; j++) {
+            int acum = 0;
+            for (int k = 0; k < 3; k++)
+                acum += m1[i][k] * m2[k][j];
+            resultado[i][j] = acum;
+        }
+    }
+
+    int v1[5];
+    for (int i = 0; i < 5; i++) {
+        cout << "Valor da posição " << i << ":\n";
+        cin >> v1[i];
+    }
+
+    int max, min;
+    max = min = v1[0]; // Quais valores?
+    for (int i = 1; i < 5; i++) {
+        if (v1[i] < min) min = v1[i];
+        if (v1[i] > max) max = v1[i];
+    }
+    cout << "Maior: " << max << "  Menor: " << min << "\n";
+
+    int pmax, pmin;
+    pmax = pmin = 0; // Onde estão os valores?
+    for (int i = 1; i < 5; i++) {
+        if (v1[i] < v1[pmin]) pmin = i;
+        if (v1[i] > v1[pmax]) pmax = i;
+    }
+    cout << "Maior: " << v1[pmax] << "  Menor: " << v1[pmin] << "\n";
+
+    bubble(v1, 5);
+    for (int i = 0; i < 5; i++) cout << i << " : " << v1[i] << "\n";
+
+
+}
+
---
+++ classes/Aula20220312/BuscaTexto/BuscaTexto/BuscaTexto.cpp
@@ -0,0 +1,68 @@
+// BuscaTexto.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+#include <chrono>
+#include <time.h>
+#include <ctime>
+
+using std::chrono::milliseconds;
+using std::chrono::duration_cast;
+using std::chrono::system_clock;
+
+long getMilisegundos() {
+    return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+int buscar_forca_bruta(char t[], int sizet, char p[], int sizep) {
+    for (int i = 0; i <= sizet - sizep; i++) {
+        int j = 0;
+        while ((j < sizep) && (t[i + j] == p[j]))
+            j++;
+        if (j == sizep)
+            return i;
+    }
+    return -1;
+}
+
+int buscar_boyer_moore(char t[], int sizet, char p[], int sizep) {
+    // Compara a partir do final
+    // Se encontra incompatibilidade, busca o incompativel no restante do texto
+    // 1 - Encontrou, alinha com o texto
+    // 2 - Não encontrou, salta para a posição após a incompatibilidade
+    int i = 0;
+    while (i <= sizet - sizep) {
+        int pos = sizep - 1;
+        while ((pos > -1) && (t[i + pos] == p[pos]))
+            pos--;
+        if (pos == -1) // Encontrou
+            return i;
+        else {
+            char c = t[i+pos];
+            int posDif = pos;
+            // Tenta achar o caractere no trecho anterior
+            do {
+                pos--;
+            } while ((pos > -1) && (c != p[pos]));
+                
+            if (pos == -1) // Não encontrou, desloca após a diferença
+                i += (posDif + 1);
+            else         // Encontrou, alinha com equivalente 
+                i += (posDif - pos);
+        }
+    }
+    return -1;
+}
+
+
+
+int main()
+{
+    char texto[]  = "GCAATGCCTATGTGACCGAT";
+    char padrao[] = "CGAT";
+
+    std::cout << "FB: " << buscar_forca_bruta(texto, 20, padrao, 4) << "\n";
+
+    std::cout << "BM: " << buscar_boyer_moore(texto, 20, padrao, 4) << "\n";
+}
+
